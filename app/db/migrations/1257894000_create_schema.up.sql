881eb2016f530986ceaf3b8e547f25d7ee4ad6e6
Annie Munoz <amunoz@truora.com>
2023-12-18 12:16:22 -0500

    fix migrations make prepare now works

--- app/db/migrations/1257894000_create_schema.up.sql
+++ app/db/migrations/1257894000_create_schema.up.sql
@@ -1,3 +1,9 @@
+DROP TABLE IF EXISTS `battles`;
+DROP TABLE IF EXISTS `monsters`;
+
+DROP INDEX IF EXISTS `idx_battles_deleted_at`;
+DROP INDEX IF EXISTS `idx_monsters_deleted_at`;
+
 CREATE TABLE `battles` (
   `id` integer,
   `monster_a` integer,
